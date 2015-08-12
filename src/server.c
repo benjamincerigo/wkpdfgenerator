99699ed518d374f6eedb6aad075eafb830d056ad
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2015-08-12 07:47:08 +0200

    Port number is a constant

--- src/server.c
+++ src/server.c
@@ -8,28 +8,25 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+const int portno = 9200;
+
 void error(const char *msg)
 {
     perror(msg);
     exit(1);
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-     int sockfd, newsockfd, portno;
+     int sockfd, newsockfd;
      socklen_t clilen;
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
      int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
